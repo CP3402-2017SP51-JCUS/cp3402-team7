73f78c9af1f787bfba9d90d9bcd50c198e5e0aee
Ashish Jacob <ashishjohnsonjacob@outlook.com>
2017-04-28 23:50:11 +0800

    Edited HEADER
    Add background image to Header, Included Title and Description.

--- themes/CustomJuice/header.php
+++ themes/CustomJuice/header.php
@@ -1,18 +1,43 @@
 <html>
 <head>
-<title>Tutorial theme</title>
+<title>Fresh Juice Singapore</title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<style>
+#header
+{
+  background: url("<?php bloginfo('stylesheet_directory'); ?>/theme_images/fresh_juice.png")
+  repeat bottom center;
+}
+/*
+#icon
+{
+  height: 200px;
+  width: 200px;
+  background: url("<?php bloginfo('stylesheet_directory'); ?>/theme_images/icon.png");
+}
+*/
+</style>
 </head>
 <body>
 <div id="wrapper">
-<div id="header">
-<h1>HEADER</h1>
-<nav class="skew-menu">
-<ul>
-  <li><a href="#">Home</a></li>
-  <li><a href="#">About</a></li>
-  <li><a href="#">Blog</a></li>
-  <li><a href="#">Contacts</a></li>
-</ul>
-</nav>
+  <div id="header">
+   <!-- <div id="icon"> </div>  //-->
+      <div class="header_title">
+       <h1>
+        <a href="<?php echo get_option('home'); ?>">
+           <?php bloginfo('name'); ?></a>
+       </h1>
+      </div>
+       <div class="description">
+         <?php bloginfo('description'); ?>
+       </div>
+    <nav class="skew-menu">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contacts</a></li>
+      </ul>
+    </nav>
+  </div>
 </div>
