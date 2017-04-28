bce1d234359a2c2d208ce711e256d15383e6947e
GlebTsoy <everlivingtsoy@gmail.com>
2017-04-28 17:50:30 +0800

    Start of new theme

---
+++ themes/CustomJuice/header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Tutorial theme</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+<h1>HEADER</h1>
+<nav class="skew-menu">
+<ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">About</a></li>
+  <li><a href="#">Blog</a></li>
+  <li><a href="#">Contacts</a></li>
+</ul>
+</nav>
+</div>
