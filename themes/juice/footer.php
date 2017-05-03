baf68b9438beb1c6ee9621970bfcfb4b1d70b9ed
Chrisleepin <nobleelement@yahoo.com.sg>
2017-05-03 12:11:59 +0800

    Footer CSS Style Created
    Created the footer CSS in style.css, its called colophon

--- themes/juice/footer.php
+++ themes/juice/footer.php
@@ -13,9 +13,9 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo" href="style.css">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'juice' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'juice' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'juice' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by WordPress', 'juice' )); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'juice' ), 'juice', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
