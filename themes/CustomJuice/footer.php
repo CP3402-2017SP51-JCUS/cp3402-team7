4cad59d34948fe11619e8f99dad0159be51d899a
Chrisleepin <nobleelement@yahoo.com.sg>
2017-05-17 12:14:23 +0800

    Footer "split" to two
    The footer has a left div and a right div

--- themes/CustomJuice/footer.php
+++ themes/CustomJuice/footer.php
@@ -1,8 +1,22 @@
 </div>
 	<footer>
-    	<div id="footer" href="style.css">
-			Footer
+    	<div id="footer">
+		<div id="left">
+		WP
+		<br>
+		About Us
+		<br>
+		Contact Us
+		<br>
+		Terms of Service
+		<br>
+		Private Policy
+		</div>
+		<div id="right">
+		Articles
+		</div>
         </div>
+	Proudly powered by WordPress
 	</footer>
 </body>
 </html>
